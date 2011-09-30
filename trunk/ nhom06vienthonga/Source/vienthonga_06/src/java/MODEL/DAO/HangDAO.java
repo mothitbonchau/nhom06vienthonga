af13ce05b60b6b9b801901e59363ebb3ca08ee07
saucon89@gmail.com <saucon89@gmail.com@f5b79a5f-bb7e-6306-395e-6f76b76cfb08>
2011-09-30 04:25:31 +0000

    Bình - Commit lấy danh sách hãng theo Hibernate + up folder hình ảnh

---
+++ trunk/ nhom06vienthonga/Source/vienthonga_06/src/java/MODEL/DAO/HangDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODEL.DAO;
+
+import MODEL.POJO.Hang;
+
+import MODEL.UTIL.HibernateUtil;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.util.MySqlDataAccessHelper;
+import org.hibernate.Hibernate;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public class HangDAO {
+    public static List<Hang> LayHang()
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        List<Hang> list = null;        
+
+        try {
+            String hql = "FROM Hang WHERE tinhTrang = 0";
+            Query query = session.createQuery(hql);
+            //query.setInteger("tt", 0);
+            list = query.list();
+        } catch (Exception ex) {
+            Logger.getLogger(HangDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally
+        {
+            session.close();
+        }
+        
+        return list;
+    }
+}
